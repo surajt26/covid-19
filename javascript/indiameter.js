0fecb8b929d4e3f66bb633b407e438951b1026f7
Suraj KY <face.surajyadav@gmail.com>
2021-02-03 19:46:06 +0530

    whole project

---
+++ javascript/indiameter.js
@@ -0,0 +1,369 @@
+document.getElementById("btn01").addEventListener("click",function(){
+    var box01 = document.getElementById("div01");
+    if(box01.style.display=="block"){
+        box01.style.display="none";
+    }
+    else{
+        box01.style.display="block";
+    }
+})
+/document.getElementById("btn02").addEventListener("click",function()
+{
+    var box02 = document.getElementById("div02");
+    if(box02.style.display=="block")
+    {
+        box02.style.display="none";
+    }
+    else
+    {
+        box02.style.display="block";
+    }
+})
+document.getElementById("btn03").addEventListener("click",function()
+{
+    var box03 = document.getElementById("div03");
+    if(box03.style.display=="block")
+    {
+        box03.style.display="none";
+    }
+    else
+    {
+        box03.style.display="block";
+    }
+})
+document.getElementById("btn04").addEventListener("click",function()
+{
+    var box04 = document.getElementById("div04");
+    if(box04.style.display=="block")
+    {
+        box04.style.display="none";
+    }
+    else
+    {
+        box04.style.display="block";
+    }
+})
+document.getElementById("btn05").addEventListener("click",function()
+{
+    var box05 = document.getElementById("div05");
+    if(box05.style.display=="block")
+    {
+        box05.style.display="none";
+    }
+    else
+    {
+        box05.style.display="block";
+    }
+})
+document.getElementById("btn06").addEventListener("click",function()
+{
+    var box06 = document.getElementById("div06");
+    if(box06.style.display=="block")
+    {
+        box06.style.display="none";
+    }
+    else
+    {
+        box06.style.display="block";
+    }
+})
+document.getElementById("btn07").addEventListener("click",function()
+{
+    var box07 = document.getElementById("div07");
+    if(box07.style.display=="block")
+    {
+        box07.style.display="none";
+    }
+    else
+    {
+        box07.style.display="block";
+    }
+})
+document.getElementById("btn08").addEventListener("click",function()
+{
+    var box08 = document.getElementById("div08");
+    if(box08.style.display=="block")
+    {
+        box08.style.display="none";
+    }
+    else
+    {
+        box08.style.display="block";
+    }
+})
+document.getElementById("btn09").addEventListener("click",function()
+{
+    var box09 = document.getElementById("div09");
+    if(box09.style.display=="block")
+    {
+        box09.style.display="none";
+    }
+    else
+    {
+        box09.style.display="block";
+    }
+})
+document.getElementById("btn10").addEventListener("click",function()
+{
+    var box10 = document.getElementById("div10");
+    if(box10.style.display=="block")
+    {
+        box10.style.display="none";
+    }
+    else
+    {
+        box10.style.display="block";
+    }
+})
+document.getElementById("btn11").addEventListener("click",function()
+{
+    var box11 = document.getElementById("div11");
+    if(box11.style.display=="block")
+    {
+        box11.style.display="none";
+    }
+    else
+    {
+        box11.style.display="block";
+    }
+})
+document.getElementById("btn12").addEventListener("click",function()
+{
+    var box12 = document.getElementById("div12");
+    if(box12.style.display=="block")
+    {
+        box12.style.display="none";
+    }
+    else
+    {
+        box12.style.display="block";
+    }
+})
+document.getElementById("btn13").addEventListener("click",function()
+{
+    var box13 = document.getElementById("div13");
+    if(box13.style.display=="block")
+    {
+        box13.style.display="none";
+    }
+    else
+    {
+        box13.style.display="block";
+    }
+})
+document.getElementById("btn14").addEventListener("click",function()
+{
+    var box14 = document.getElementById("div14");
+    if(box14.style.display=="block")
+    {
+        box14.style.display="none";
+    }
+    else
+    {
+        box14.style.display="block";
+    }
+})
+document.getElementById("btn15").addEventListener("click",function()
+{
+    var box15 = document.getElementById("div15");
+    if(box15.style.display=="block")
+    {
+        box15.style.display="none";
+    }
+    else
+    {
+        box15.style.display="block";
+    }
+})
+document.getElementById("btn16").addEventListener("click",function()
+{
+    var box16 = document.getElementById("div16");
+    if(box16.style.display=="block")
+    {
+        box16.style.display="none";
+    }
+    else
+    {
+        box16.style.display="block";
+    }
+})
+document.getElementById("btn17").addEventListener("click",function()
+{
+    var box17 = document.getElementById("div17");
+    if(box17.style.display=="block")
+    {
+        box17.style.display="none";
+    }
+    else
+    {
+        box17.style.display="block";
+    }
+})
+document.getElementById("btn18").addEventListener("click",function()
+{
+    var box18 = document.getElementById("div18");
+    if(box18.style.display=="block")
+    {
+        box18.style.display="none";
+    }
+    else
+    {
+        box18.style.display="block";
+    }
+})
+document.getElementById("btn19").addEventListener("click",function()
+{
+    var box19 = document.getElementById("div19");
+    if(box19.style.display=="block")
+    {
+        box19.style.display="none";
+    }
+    else
+    {
+        box19.style.display="block";
+    }
+})
+document.getElementById("btn20").addEventListener("click",function()
+{
+    var box20 = document.getElementById("div20");
+    if(box20.style.display=="block")
+    {
+        box20.style.display="none";
+    }
+    else
+    {
+        box20.style.display="block";
+    }
+})
+document.getElementById("btn21").addEventListener("click",function()
+{
+    var box21 = document.getElementById("div21");
+    if(box21.style.display=="block")
+    {
+        box21.style.display="none";
+    }
+    else
+    {
+        box21.style.display="block";
+    }
+})
+document.getElementById("btn22").addEventListener("click",function()
+{
+    var box22 = document.getElementById("div22");
+    if(box22.style.display=="block")
+    {
+        box22.style.display="none";
+    }
+    else
+    {
+        box22.style.display="block";
+    }
+})
+document.getElementById("btn23").addEventListener("click",function()
+{
+    var box23 = document.getElementById("div23");
+    if(box23.style.display=="block")
+    {
+        box23.style.display="none";
+    }
+    else
+    {
+        box23.style.display="block";
+    }
+})
+document.getElementById("btn24").addEventListener("click",function()
+{
+    var box24 = document.getElementById("div24");
+    if(box24.style.display=="block")
+    {
+        box24.style.display="none";
+    }
+    else
+    {
+        box24.style.display="block";
+    }
+})
+document.getElementById("btn25").addEventListener("click",function()
+{
+    var box25 = document.getElementById("div25");
+    if(box25.style.display=="block")
+    {
+        box25.style.display="none";
+    }
+    else
+    {
+        box25.style.display="block";
+    }
+})
+document.getElementById("btn26").addEventListener("click",function()
+{
+    var box26 = document.getElementById("div26");
+    if(box26.style.display=="block")
+    {
+        box26.style.display="none";
+    }
+    else
+    {
+        box26.style.display="block";
+    }
+})
+document.getElementById("btn27").addEventListener("click",function()
+{
+    var box27 = document.getElementById("div27");
+    if(box27.style.display=="block")
+    {
+        box27.style.display="none";
+    }
+    else
+    {
+        box27.style.display="block";
+    }
+})
+document.getElementById("btn28").addEventListener("click",function()
+{
+    var box28 = document.getElementById("div28");
+    if(box28.style.display=="block")
+    {
+        box28.style.display="none";
+    }
+    else
+    {
+        box28.style.display="block";
+    }
+})
+document.getElementById("btn29").addEventListener("click",function()
+{
+    var box29 = document.getElementById("div29");
+    if(box29.style.display=="block")
+    {
+        box29.style.display="none";
+    }
+    else
+    {
+        box29.style.display="block";
+    }
+})
+document.getElementById("btn30").addEventListener("click",function()
+{
+    var box30 = document.getElementById("div30");
+    if(box30.style.display=="block")
+    {
+        box30.style.display="none";
+    }
+    else
+    {
+        box30.style.display="block";
+    }
+})
+document.getElementById("btn31").addEventListener("click",function()
+{
+    var box31 = document.getElementById("div31");
+    if(box31.style.display=="block")
+    {
+        box31.style.display="none";
+    }
+    else
+    {
+        box31.style.display="block";
+    }
+})
